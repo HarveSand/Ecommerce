b63ac1d639b2a4848969fd3e9024834fe03dd5f1
Harve Sand <59222085+HarveSand@users.noreply.github.com>
2022-03-17 20:23:53 +0800

    Add files via upload

---
+++ config/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+define('HOST', 'localhost');
+define('USER', 'id18606094_root');
+define('PASSWORD', 'aPG0a+wqaLEg*yXS');
+define('DATABASE_NAME', 'id18606094_ecommerceapp');
+
+define('CURRENCY', 'P');
+
+
+
+?>
